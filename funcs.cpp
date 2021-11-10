0569a482b735fed281849cc40f2ed1b2d9831b2d
jiangr0421@gmail.com <ryanjiang@DESKTOP-109BKGG.localdomain>
2021-11-10 17:19:48 -0500

    Part A-C

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,30 @@
 #include <iostream>
+#include <cmath>
 #include "funcs.h"
+#include "coord3d.h"
 
-// add functions here
+double length(Coord3D *p){
+  double x = (*p).x;
+  double y = (*p).y;
+  double z = (*p).z;
 
+  return (sqrt(x * x + y * y + z * z));
+}
+
+Coord3D * fartherFromOrigin(Coord3D *p1, Coord3D *p2){
+  double length_p1 = length(p1);
+  double length_p2 = length(p2);
+
+  if(length_p1 < length_p2){
+    return p2;
+  }
+  else{
+    return p1;
+  }
+}
+
+void move(Coord3D *ppos, Coord3D *pvel, double dt){
+  (*ppos).x += (*pvel).x * dt;
+  (*ppos).y += (*pvel).y * dt;
+  (*ppos).z += (*pvel).z * dt;
+}
--- main.cpp
+++ main.cpp
@@ -1,9 +1,32 @@
 #include <iostream>
 
-
+#include "coord3d.h"
 #include "funcs.h"
 
 int main()
 {
+  Coord3D pointP = {10, 20, 30};
+
+  std::cout << "Part A:\nLength of P: " << length(&pointP) << "\n\n";
+
+  Coord3D pointQ = {-20, 21, -22};
+  
+  std::cout << "Part B:\n";
+  std::cout << "Address of P: " << &pointP << "\n";
+  std::cout << "Address of Q: " << &pointQ << "\n";
+
+  std::cout << "Farther from origin: " << fartherFromOrigin(&pointP, &pointQ) << "\n\n";
+
+  Coord3D pos = {0, 0, 100.0};
+  Coord3D vel = {1, -5, 0.2};
+
+  std::cout << "Part C:\n";
+  std::cout << "Original: " << pos.x << " " << pos.y << " " << pos.z << "\n";
+  move(&pos, &vel, 2.0);
+  std::cout << "After move: " << pos.x << " " << pos.y << " " << pos.z << "\n";
+
+
+
+
   return 0;
 }
